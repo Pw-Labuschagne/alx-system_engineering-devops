17ed1cc5634815d40dc74cf9b6f91205fe527d12
Pw-Labuschagne <pw.labuschagne@yahoo.com>
2022-12-12 17:47:14 +0200

    Configuration_management

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,10 @@
 #Create a file in /tmp folder
 
-file{ 'school':
+file { 'school':
   ensure  => 'present',
-  content => 'I love puppet',
+  content => 'I love Puppet',
   group   => 'www-data',
-  owner   => 'www-data',
   mode    => '0744',
+  owner   => 'www-data',
   path    => '/tmp/school',
 }
--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
-#Install package flask from pip3 with puppet
-
-package { 'flask'
+#!/usr/bin/pup
+# Install version of flask (2.1.0)
+package {'flask':
   ensure   => '2.1.0',
   provider => 'pip3'
 }
